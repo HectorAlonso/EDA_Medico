cae76ac6138276a29079f5269efebca10b617486
Hector Alonso <aloarenas98@gmail.com>
2025-05-12 16:05:41 -0600

    Carga de Datos

---
+++ DataMedic.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# ! 1- CARGA DE DATOS ---------------------------------------------------------------------
+
+datos = pd.read_csv("Medicaldataset.csv")
