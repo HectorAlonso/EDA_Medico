ba93729dbefd9012833c8e3199e5803df21442d3
Hector Alonso <aloarenas98@gmail.com>
2025-05-12 16:16:29 -0600

    Visualizaciones

--- DataMedic.py
+++ DataMedic.py
@@ -42,3 +42,36 @@ datos.loc[datos['Heart rate']==1111, 'Heart rate'] = 111
 min = datos['Heart rate'].min()
 maximo = datos['Heart rate'].max()      
 print(f"El rango del ritmo cardiaco es: {min} - {maximo}") 
+
+# ! 3- VISUALIZACIONES ---------------------------------------------------------------------
+
+# ? Histograma de presion sistolica ---------------------------------------------------------
+
+sns.histplot(data=datos['Systolic blood pressure'])
+plt.xlabel("Presion Sistolica")
+plt.ylabel("Cantidad de Personas")
+plt.show()
+
+# ? Boxplot de troponina según diagnóstico. -----------------------------------------------
+
+sns.boxplot(x='Result', y='Troponin', data=datos)
+plt.title('Distribución de niveles de Troponina según Diagnóstico')
+plt.show()
+
+# ? Grafico de barras: numero de positivos y negativos por grupo de edad: <30, 30-50, >50 --
+
+bins = [0,30,51,105]
+labels = ['<30','30-50','>50']
+datos['AgeGroup'] = pd.cut(datos['Age'], bins=bins, labels=labels, right=False)
+
+CantResultadoXEdad = datos.groupby(['AgeGroup','Result']).size().unstack()
+CantResultadoXEdad.plot(kind='bar', stacked=False, figsize=(8,6))
+
+plt.title('Numero de Diagnosticos por Grupo de Edad')
+plt.xlabel('Grupos de Edad')
+plt.ylabel('Numero de Casos')
+plt.grid(color='red',axis='y', linestyle='--', alpha=0.8)
+plt.legend(title='Diagnostico')
+plt.tight_layout()
+plt.xticks(rotation=0)
+plt.show()
